989f4ec28ddefc587f3d213434bb4f699a1f012f
Ken Moore <moorekou@gmail.com>
2025-05-09 17:10:52 -0400

    Setup Gate error handling

--- src-go/gate_arm64.go
+++ src-go/gate_arm64.go
@@ -13,15 +13,26 @@ type GateConfig struct {
 	GpioPin uint8 `json:"gpio_num"`
 }
 
-func (gc *GateConfig) OpenGate() {
+func (gc *GateConfig) OpenGate() (err error) {
 	if gc == nil || gc.GpioPin < 1 {
 		fmt.Println("No Gate configured!!")
-		return
+		return fmt.Errorf("No Gate Configured")
 	}
+	//Setup crash handling for error detection. rpio library panics on errors
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Error opening gate:", r)
+			err = fmt.Errorf("Error opening gate")
+		}
+	}()
+
+	//Need to toggle the pin as an output first
+	rpio.PinMode(gc.GpioPin, rpio.Output)
 	pin := rpio.Pin(gc.GpioPin)
 	//Gate is triggered to open by a 1 second state change on the pin
-	pin.Toggle()
+	pin.PullUp()
 	time.Sleep(time.Second) //wait one second
-	pin.Toggle()
+	pin.PullDown()
 	rpio.Close()
+	return nil
 }
