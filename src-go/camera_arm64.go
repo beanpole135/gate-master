a6b9bcb25920fbbb1bbcec15b2b865fcf7aaad8c
Ken Moore <moorekou@gmail.com>
2025-05-16 20:38:45 -0400

    Add initial support for 90-degree camera rotations

--- src-go/camera_arm64.go
+++ src-go/camera_arm64.go
@@ -3,18 +3,27 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"image"
+	"image/jpeg"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"net/textproto"
 	"os"
+	"syscall"
 
 	"github.com/cleroux/go-rpicamvid"
+	"github.com/disintegration/imaging"
 )
 
 type Camera struct {
 	//CamDevice *device.Device
-	err    error
-	webcam *rpicamvid.Rpicamvid
+	err      error
+	webcam   *rpicamvid.Rpicamvid
+	rotation int `json:"-"` //internal tag for 90 degree rotations
 }
 
 type CamConfig struct {
@@ -28,9 +37,13 @@ func NewCamera(cc CamConfig) (*Camera, error) {
 	// Now initialize the camera
 	l := log.New(os.Stdout, "", log.LstdFlags)
 	var opts []string
-	if cc.Rotation != 0 {
-		opts = append(opts, "--rotation", fmt.Sprintf("%d", cc.Rotation))
+	if cc.Rotation >= 180 {
+		opts = append(opts, "--rotation", fmt.Sprintf("%d", 180)) //This flag only supports 0 or 180 degrees
+		C.rotation = cc.Rotation - 180
+	} else {
+		C.rotation = cc.Rotation
 	}
+
 	C.webcam = rpicamvid.New(l, cc.Width, cc.Height, opts...)
 	// Start it up and see if it is working (then close it down again)
 	stream, err := C.webcam.Start()
@@ -73,5 +86,90 @@ func (C *Camera) TakePicture() []byte {
 		return []byte{}
 	}
 	defer fr.Close()
-	return fr.GetBytes()
+	return C.processImage(fr.GetBytes())
+}
+
+func (C *Camera) processImage(frame []byte) []byte {
+	if C.rotation == 90 {
+		img, _, err := image.Decode(bytes.NewReader(frame))
+		if err == nil {
+			img := imaging.Rotate90(img)
+			bytes.NewWriter
+			buf := new(bytes.Buffer)
+			err = jpeg.Encode(buf, img, nil)
+			if err != nil {
+				fmt.Println("Error encoding jpeg image (image not rotated 90 degrees):", err)
+				C.rotation = 0 //Skip this logic next time - it will not work
+			} else {
+				frame = buf.Bytes()
+			}
+		} else {
+			fmt.Println("Error decoding jpeg image (image not rotated 90 degrees):", err)
+			C.rotation = 0 //Skip this logic next time - it will not work
+		}
+	}
+
+	return frame
+}
+
+func (C *Camera) serveHttp(w http.ResponseWriter, req *http.Request) {
+	// This was a blanket copy of the go-rpicamvid function called "HTTPHandler" (in http.go) - 5/16/2025
+	// Just so that we could inject the "processImage()" function above into it for additional 90-degree rotations
+	stream, err := C.webcam.Start()
+	if err != nil {
+		C.webcam.log.Printf("Failed to start camera: %v\n", err)
+		http.Error(w, "Failed to start camera: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer stream.Close()
+
+	mimeWriter := multipart.NewWriter(w)
+	defer mimeWriter.Close()
+	contentType := fmt.Sprintf("multipart/x-mixed-replace; boundary=%s", mimeWriter.Boundary())
+	w.Header().Set("Content-Type", contentType)
+
+	partHeader := make(textproto.MIMEHeader, 1)
+	partHeader.Add("Content-Type", "image/jpeg")
+
+	ctx := req.Context()
+
+	for {
+		if ctx.Err() != nil {
+			return
+		}
+
+		err := func() error {
+			f, err := stream.GetFrame()
+			if err != nil {
+				C.webcam.log.Printf("Failed to get camera frame: %v\n", err)
+				return nil // continue for loop
+			}
+			defer f.Close()
+
+			partWriter, err := mimeWriter.CreatePart(partHeader)
+			if err != nil {
+				C.webcam.log.Printf("Failed to create multi-part section: %v\n", err)
+				return err
+			}
+
+			if _, err := partWriter.Write(C.processImage(f.GetBytes())); err != nil {
+				if errors.Is(err, syscall.EPIPE) {
+					// Client went away
+					return err
+				}
+
+				switch err.Error() {
+				case "http2: stream closed", "client disconnected":
+					// Client went away
+					return err
+				}
+				C.webcam.log.Printf("Failed to write video frame: %v\n", err)
+				return err
+			}
+			return nil
+		}()
+		if err != nil {
+			break
+		}
+	}
 }
